cb6a6a1e168fef12511d494275fbf8cedca26881
VedatOlmez <vedatcenin@gmail.com>
2020-12-20 23:39:26 +0300

    google search console

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: "G-BT79WW9GL9",
+        trackingId: "UA-121912050-7",
         head: true,
       },
     },
